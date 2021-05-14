2242b695a37b072f23d78b3fee91fd12b9db9c2c
Simon Peters <simon@spaced.be>
2021-05-14 16:33:36 +0200

    add experimental tag

--- apps/tuskdesk/next.config.js
+++ apps/tuskdesk/next.config.js
@@ -3,6 +3,7 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
   nx: {
+    target: 'experimental-serverless-trace',
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
