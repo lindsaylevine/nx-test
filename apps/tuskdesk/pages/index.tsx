042b554eee863c935aff9cacc3f4297be0571a79
Simon Peters <simon@spaced.be>
2021-05-18 15:51:01 +0200

    add getStaticProps

--- apps/tuskdesk/pages/index.tsx
+++ apps/tuskdesk/pages/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import styled from '@emotion/styled';
 import {useEffect, useState} from "react";
 
+export function getStaticProps(context) {
+  const data = [];
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
+
 export function Index({data}) {
   console.log(data);
   /*
