96461de2461e18dde62490b78689902422644670
Lindsay Levine <lindslev.ll@gmail.com>
2021-06-10 21:59:45 -0400

    force nx to work lol

--- apps/tuskdesk/next.config.js
+++ apps/tuskdesk/next.config.js
@@ -1,9 +1,13 @@
+// module.exports = {
+//   target: 'serverless'
+// }
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
+  target: 'serverless',
+  distDir: './out/.next',
   nx: {
-    target: 'experimental-serverless-trace',
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
---
+++ next.config.js
@@ -0,0 +1,6 @@
+
+            module.exports = {
+              target: 'serverless',
+              distDir: './apps/tuskdesk/out/.next'
+            }
+
